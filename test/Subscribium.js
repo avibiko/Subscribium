0d75c8af86e24db35bb66e44f9400404957c9327
nimrod feldman <nimrod@aireye.tech>
2022-04-20 18:56:49 +0300

    added tests

--- test/Subscribium.js
+++ test/Subscribium.js
@@ -6,6 +6,7 @@ contract('Subscribium', (accounts) => {
     let subsInstance;
     let tokenInstance;
     let publisher;
+    let subscriber = accounts[0];
     beforeEach('should setup the contract instance', async () => {
         subsInstance = await subs.deployed();
         tokenInstance = await token.deployed();
@@ -26,10 +27,23 @@ contract('Subscribium', (accounts) => {
         await subsInstance.Subscribe(interval, value)
 
         const subscribers = await subsInstance.subscribers;
-        const subscription = await subscribers(accounts[0]);
+        const subscription = await subscribers(subscriber);
         console.log("subsInstance.subscribers",  subscription.interval.words[0]);
 
         assert.equal(subscription.interval.words[0], interval, 'interval is false');
         assert.equal(subscription.value.words[0], value, 'value is false');
+    });
+
+    it("should pay only on duedate", async () => {
+        const subscribers = await subsInstance.subscribers;
+        const subscription = await subscribers(subscriber);
+        
+        const interval = subscription.interval.words[0];
+
+        let result = await subsInstance.ExecuteSubscription(subscriber);
+        assert.equal(result, true, "subscription executed 1");
+
+        result = await subsInstance.ExecuteSubscription(subscriber);
+        assert.equal(result, false, "subscription executed 2");
     });
 });
