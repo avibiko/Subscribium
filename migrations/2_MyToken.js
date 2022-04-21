5113b0d66f4665ce338109f2251e05829849c0b3
Avishay Bikowsky <avishayb@next-insurance.com>
2022-04-21 21:47:16 +0300

    fix test with try catch

--- migrations/2_MyToken.js
+++ migrations/2_MyToken.js
@@ -2,7 +2,7 @@ const MyToken = artifacts.require("MyToken");
 const Subscribium = artifacts.require("Subscribium");
 
 module.exports = function (deployer) {
-  deployer.deploy(MyToken, 100).then(function() {
-    return deployer.deploy(Subscribium, "0x6b67d934c4cef8f4bce9b81d05cb2a9bbcba3697", MyToken.address)
+  deployer.deploy(MyToken, 10000).then(function() {
+    return deployer.deploy(Subscribium, "0x9fc43f48dD0838cd4B6FCDA717DF1fa249EC2444", MyToken.address)
   });
 };
